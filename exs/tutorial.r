4d5dd817fba164772028cf326ea58ea2fa0ff0a2
Pesteves2002 <tomasesteves2002@gmail.com>
2022-03-07 18:37:30 +0000

    Add tutorial

---
+++ exs/tutorial.r
@@ -0,0 +1,8 @@
+dataGALP = read.csv("//home/tomas/Documents/PE/data/GALP.txt", header=TRUE, sep="", dec=",")
+dataGALP = dataGALP[,5]
+dataGALP=ts(dataGALP)
+ts.plot(dataGALP, xlab="Period: 2019-2021", ylab="Price", main="Closing Values Share Prices (GALP
+ENERGIA)")
+logreturnGALP = as.ts(diff(log(dataGALP)))
+ts.plot(logreturnGALP, xlab="Period: 2019-2021", ylab="log-returns", main="Log-RETURNS (GALP
+ENERGIA)")
