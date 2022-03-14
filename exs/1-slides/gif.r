b641865aac66d7c884f278827f265cfac0f64631
Pesteves2002 <tomasesteves2002@gmail.com>
2022-03-14 14:26:38 +0000

    Add gif (incomplete)

---
+++ exs/1-slides/gif.r
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(openxlsx)
+library(gganimate)
+
+
+mortes <- read.xlsx("./data/MortesPortugal.xlsx")
+
+graph2 <- ggplot(data = mortes, aes(x = date, y = new_deaths)) +
+    geom_line() +
+    geom_point(colour = "blue") +
+    transition_reveal(along = date)
+
+
+anim_save("ps3 game sales.gif")
