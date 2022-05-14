9ad3c42d99f05be5c22b061148774cb7013f4889
Pesteves2002 <tomasesteves2002@gmail.com>
2022-05-14 11:30:32 +0100

    Finish exercise 9

--- exs/Proj/ex9.R
+++ exs/Proj/ex9.R
@@ -1,46 +1,43 @@
+library(ggplot2)
 set.seed(361)
 
 lambda <- 0.01
-
 confidence <- 0.98
-
-deviation <- sqrt(1/(lambda^2))
-
+m <- 650
 
 # Intervalo de confiança
 
 # CI = confidence * (standart deviation/sqrt(n))
-
-CIcalc <- function(lower, upper)
+CIcalc <- function(vector)
 {
-  return (confidence* (deviation/sqrt(n))*2 +  upper - lower)
-  
-  
+  len <- length(vector)
+  sd_vec <- sd(vector)
+  value <- qt((confidence + 1)/2, df = n - 1)
+  return (value * sd_vec/sqrt(len)*2)
 }
+
 
 index <- numeric()
 values <- numeric()
 
-for (i in 1:50)
+for (i in 1:150)
 {
   media <- 0
   n <- i * 100
-  for (m in 1:650){
-    
-  a<- rexp(n,1.37)
-  upper <- max(a)
-  lower <- min(a)
-  calc <- CIcalc(lower,upper)
+  for (l in 1:m){
+  vector<- rexp(n,lambda)
+  calc <- CIcalc(vector)
   media <- media + calc
   }
-  media <- media /650
+  media <- media/m
   index <- append(index,n)
   values <- append(values,media)
   
 }
 
 table <- cbind(index,values)
-
 df <- data.frame(table)
 
-ggplot(df, aes(x=index, y=values )) +  geom_bar(stat="identity", position=position_dodge())
+ggplot(df, aes(x=index, y=values )) +  geom_line(stat="identity", position=position_dodge())+
+  labs(title="Mean value of Amplitude from n = 100 to n = 5000", x= "Value of n", y = "Mean value of Amplitude")
+
